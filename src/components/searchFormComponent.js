bcee31b1992a8024a554e251752101b01a63a9bc
leooskarpa <skarpaleo111@gmail.com>
2021-05-24 23:15:51 +0200

    Search form CSS

--- src/components/searchFormComponent.js
+++ src/components/searchFormComponent.js
@@ -3,15 +3,16 @@ import favourite_icon from './images/favourite-empty.svg'
 function SearchFormComponent() {
     return (
         <div className="search-form-container">
-            <form className="search-form">
-                <input className='search-form-text' type="text" placeholder="Search" />
+            <div className="search-form-grid">
+                <form className="search-form">
+                    <input className='search-form-text' type="text" placeholder="Search" />
 
-                <input className='search-form-button' type="submit" value="Search" />
-
+                    <input className='search-form-button' type="submit" value="Search" />
+                </form>
                 <div className="favourites-container">
                     <img className="favourite-icon" src={favourite_icon} alt="Favourites" />
                 </div>
-            </form>
+            </div>
         </div>
     );
 }
